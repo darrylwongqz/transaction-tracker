0b708ae486e7fbd1d2d7b0367d2933a937f74f9a
darrylwongqz <dwqz92@gmail.com>
2024-11-15 02:00:49 +0800

    added Dockerfile and simple dockercompose + got the app connected to the mysql db + validation schema wijth joi package

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+# Stage 1: Build the NestJS application
+FROM node:22-alpine AS builder
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+RUN npm run build
+
+# Stage 2: Production environment
+FROM node:22-alpine
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+RUN npm install --only=production
+
+COPY --from=builder /usr/src/app/dist ./dist
+
+COPY .env ./
+
+EXPOSE 3000
+
+CMD ["node", "dist/main.js"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { getDatabaseConfig } from './config/database-config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Make ConfigModule available throughout the app
+      envFilePath: '.env', // Load environment variables from .env
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.string().default('localhost'),
+        DATABASE_PORT: Joi.number().default(3306),
+        DATABASE_USERNAME: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getDatabaseConfig, // Use the getDatabaseConfig function
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
