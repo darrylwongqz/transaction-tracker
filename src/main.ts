5394c06a1789c66743e41499d54a8b43f3f3bb0e
darrylwongqz <dwqz92@gmail.com>
2024-11-15 18:48:21 +0800

    added swagger integration in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Blockchain Transactions Tracker - Uniswap Transactions API')
+    .setDescription(
+      'API for tracking Uniswap WETH-USDC transactions and related pricing',
+    )
+    .setVersion('1.0')
+    .addTag('transactions') // Example tag for transactions
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
