2343bc0c1ce4088d5729fca10599cc4414433988
darrylwongqz <dwqz92@gmail.com>
2024-11-19 17:57:20 +0800

    updated pricing contants

--- src/pricing/constants/provider.constants.ts
+++ src/pricing/constants/provider.constants.ts
@@ -1,6 +1,30 @@
+import { BinanceKlineParamDto } from '../../integrations/binance/dtos/request/binance-kline-param.dto';
+import { BinanceKlineResponseDto } from '../../integrations/binance/dtos/response/binance-kline-response.dto';
+/**
+ * Available Providers
+ *
+ * This object defines the available providers for fetching kline data.
+ * Add more providers as needed.
+ */
 export const PROVIDERS = {
   BINANCE: 'binance',
-  // Add more providers as needed
+  // Add more providers here (e.g., COINBASE, KRAKEN)
 } as const;
 
-export type ProviderNameType = (typeof PROVIDERS)[keyof typeof PROVIDERS]; // Type-safe provider values
+/**
+ * ProviderNameType
+ *
+ * A union type representing the keys of the `PROVIDERS` object.
+ * This ensures type safety when working with provider names.
+ */
+export type ProviderNameType = (typeof PROVIDERS)[keyof typeof PROVIDERS];
+
+/**
+ * ProviderKlineDto
+ *
+ * A type alias for the kline DTOs used by different providers.
+ * Extend this type union as more providers are added.
+ */
+export type ProviderKlineParamDto = BinanceKlineParamDto;
+
+export type ProviderKlineResponseDto = BinanceKlineResponseDto;
